29da78415b168974364cd6a1bbfb0419d05326a1
Janmeet singh <147757403+Janny-527@users.noreply.github.com>
2025-07-31 10:30:22 +0530

    Add files via upload

---
+++ types/cards.ts
@@ -0,0 +1,11 @@
+export interface CardType {
+    title: string;
+    description: string;
+}
+
+export interface RecentEventCardType {
+    date: string;
+    eventName: string;
+    link: string;
+    image: string;
+}
---
+++ types/team.ts
@@ -0,0 +1,7 @@
+export interface TeamType {
+    name: string;
+    designation: string;
+    bio: string;
+    image: string;
+    link: string;
+}
